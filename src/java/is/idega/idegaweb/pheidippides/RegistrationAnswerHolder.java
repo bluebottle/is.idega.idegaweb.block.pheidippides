54035dcfb9149a2a443978b6b047d8be4cb56e4a
bluebottle <palli@idega.com>
2012-01-17 16:23:17 +0000

    Now returning holder with bankinfo and amount and valitor url from registration. Fixed silly bug in getting race prices

---
+++ src/java/is/idega/idegaweb/pheidippides/RegistrationAnswerHolder.java
@@ -0,0 +1,36 @@
+package is.idega.idegaweb.pheidippides;
+
+import is.idega.idegaweb.pheidippides.data.BankReference;
+import is.idega.idegaweb.pheidippides.data.RegistrationHeader;
+
+public class RegistrationAnswerHolder {
+	private BankReference bankReference;
+	private RegistrationHeader header;
+	private int amount;
+	private String valitorURL;
+	
+	public BankReference getBankReference() {
+		return bankReference;
+	}
+	public void setBankReference(BankReference bankReference) {
+		this.bankReference = bankReference;
+	}
+	public RegistrationHeader getHeader() {
+		return header;
+	}
+	public void setHeader(RegistrationHeader header) {
+		this.header = header;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public String getValitorURL() {
+		return valitorURL;
+	}
+	public void setValitorURL(String valitorURL) {
+		this.valitorURL = valitorURL;
+	}
+}
