07c4ce42eded246459f8d918f374f41b37e2fe0d
Thorhallur Helgason <laddih@gmail.com>
2012-06-13 07:01:05 +0000

    Bug fixes

--- resources/javascript/extraInformation.js
+++ resources/javascript/extraInformation.js
@@ -1,6 +1,22 @@
 jQuery.noConflict();
 
 jQuery(document).ready(function() {
+	jQuery('form.user').validate();
+	
+	jQuery('input[name="prm_estimated_time"]').mask("99:99");
+
+	jQuery('input.multiple').click(function() {
+		var input = jQuery(this);
+		var parent = input.parents('fieldset.formSection');
+		
+		if (input.is(':checked')) {
+			jQuery('input:text', parent).removeAttr('disabled');
+		}
+		else {
+			jQuery('input:text', parent).attr('disabled', 'disabled');
+		}
+	})
+	
 	jQuery('a.store').live('click', function(event) {
 		event.preventDefault();
 		var form = jQuery('form#extraInformation');
