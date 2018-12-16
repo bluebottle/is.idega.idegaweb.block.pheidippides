68dde9f55c7b9f364dc6bab3653dc6c341139094
Pall Helgason <palli@greenqloud.com>
2018-12-16 22:17:09 +0000

    Fixing positions of gift card texts and adding test class

--- src/java/is/idega/idegaweb/pheidippides/test/PDFTest.java
+++ src/java/is/idega/idegaweb/pheidippides/test/PDFTest.java
@@ -14,6 +14,12 @@ import org.ujac.util.io.FileResourceLoader;
 public class PDFTest {
 
 	public static void main(String[] args) {
+		PDFTest test = new PDFTest();
+		//test.printCertificate();
+		test.printGiftCard();
+	}
+	
+	private void printCertificate() {
 		try {
 			// defining the document properties, this map is used for dynamical content evaluation.
 			Map<String, String> documentProperties = new HashMap<String, String>();
@@ -46,6 +52,39 @@ public class PDFTest {
 		}
 		catch (DocumentHandlerException dhe) {
 			dhe.printStackTrace();
+		}		
+	}
+
+	private void printGiftCard() {
+		try {
+			// defining the document properties, this map is used for dynamical content evaluation.
+			Map<String, String> documentProperties = new HashMap<String, String>();
+			documentProperties.put("amount", "1000");
+			documentProperties.put("amountText", "Eitt þúsund krónur");
+			documentProperties.put("key", "WGNGP4SXGBLE5YQR");
+			documentProperties.put("created", "16. desember 2018");
+						
+			// instantiating the document printer
+			FileInputStream templateStream = new FileInputStream("/Users/palli/Development/illuminati/git/com.idega.block.custom/is.idega.idegaweb.block.pheidippides/resources/is_IS.locale/print/gift_card_template_2.xml");
+			DocumentPrinter documentPrinter = new DocumentPrinter(templateStream, documentProperties);
+			// in case you'd like to use a XML parser different from the default implementation
+			// you can specify it here (apache xerces in this case).
+			documentPrinter.setXmlReaderClass("org.apache.xerces.parsers.SAXParser");
+			// defining the ResourceLoader: This is necessary if you like to 
+			// dynamically load resources like images during template processing.
+			documentPrinter.setResourceLoader(new FileResourceLoader("./src/java/is/idega/idegaweb/pheidippides/test/"));
+			// generating the document output
+			FileOutputStream pdfStream = new FileOutputStream("/Users/palli/giftcard.pdf");
+			documentPrinter.printDocument(pdfStream);
 		}
+		catch (FileNotFoundException fnfe) {
+			fnfe.printStackTrace();
+		}
+		catch (IOException ie) {
+			ie.printStackTrace();
+		}
+		catch (DocumentHandlerException dhe) {
+			dhe.printStackTrace();
+		}		
 	}
 }
